4ce349d4794bac5817c711fa4543cd8a1a2ad835
hllsebastian <84684391+hllsebastian@users.noreply.github.com>
2025-07-25 21:32:38 -0500

    Logger implemented in Repositories

--- lib/data/repositories/category_repository.dart
+++ lib/data/repositories/category_repository.dart
@@ -1,16 +1,32 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:prueba_ecommerce_app/core/loggin/app_logger.dart';
 import 'package:prueba_ecommerce_app/data/api/endpoints.dart';
 
 class CategoryRepository {
+  /// Obtiene el listado de categorías desde la API.
   Future<List<String>> fetchCategories() async {
-    final response = await http.get(Uri.parse(Endpoints.categories));
+    final url = Endpoints.categories;
+    AppLogger.info("Fetching product categories from API: $url");
 
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
-      return data.cast<String>();
-    } else {
-      throw Exception('Error al cargar categorías');
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body);
+        final categories = data.cast<String>();
+
+        AppLogger.debug("Fetched ${categories.length} categories.");
+        return categories;
+      } else {
+        AppLogger.warning("Request failed with status: ${response.statusCode}");
+        throw Exception(
+          'Error ${response.statusCode}: No se pudieron cargar las categorías',
+        );
+      }
+    } catch (e, st) {
+      AppLogger.error("Exception occurred while fetching categories", e, st);
+      rethrow;
     }
   }
 }
--- lib/data/repositories/product_repository.dart
+++ lib/data/repositories/product_repository.dart
@@ -1,20 +1,37 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:prueba_ecommerce_app/core/loggin/app_logger.dart';
+import 'package:prueba_ecommerce_app/data/api/endpoints.dart';
 import 'package:prueba_ecommerce_app/domain/models/product_model.dart';
 
 class ProductRepository {
-  final String baseUrl = 'https://fakestoreapi.com';
-
   Future<List<Product>> fetchByCategory(String category) async {
-    final response = await http.get(
-      Uri.parse('$baseUrl/products/category/$category'),
-    );
+    final url = Endpoints.productsByCategory(category);
+    AppLogger.info("Fetching products from API: $url");
+    try {
+      final response = await http.get(Uri.parse(url));
 
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
-      return data.map((json) => Product.fromJson(json)).toList();
-    } else {
-      throw Exception('Error al cargar productos');
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body);
+        final products = data.map((json) => Product.fromJson(json)).toList();
+
+        AppLogger.debug(
+          "Fetched ${products.length} products for category '$category'.",
+        );
+        return products;
+      } else {
+        AppLogger.warning("Request failed with status: ${response.statusCode}");
+        throw Exception(
+          'Error ${response.statusCode}: No se pudo cargar productos',
+        );
+      }
+    } catch (e, st) {
+      AppLogger.error(
+        "Exception occurred while fetching products for category '$category'",
+        e,
+        st,
+      );
+      rethrow;
     }
   }
 }
